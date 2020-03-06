4e29307774ca04eaad6f24bba07d76837c2271fc
Abhijit Roy <dalvath3700@gmail.com>
2020-03-06 18:49:23 +0530

    dash modfied

--- libs/Dash/examples/1_demo.py
+++ libs/Dash/examples/1_demo.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['./bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
