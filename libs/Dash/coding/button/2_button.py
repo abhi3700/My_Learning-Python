4265890a614aee7243c12d310d6bdc31d33def19
Abhijit Roy <dalvath3700@gmail.com>
2021-01-20 13:02:19 +0530

    buttons added

--- libs/Dash/coding/button/2_button.py
+++ libs/Dash/coding/button/2_button.py
@@ -7,8 +7,14 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-app = dash.Dash(external_stylesheets=dbc.themes.BOOTSTRAP)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+app.layout = html.Div(
+        [
+            dbc.Button("Click me", id="example-button", className="mr-2"),
+            html.Span(id="example-output", style={"verticle-align": "middle"}),
+        ]
+    )
 
 @app.callback(
     Output("example-output", "children"),
@@ -19,14 +25,6 @@ def on_button_click(n):
         return "Not clicked."
     else:
         return f"Clicked {n} times."
-
-app.layout = html.Div(
-        [
-            dbc.Button("Click me", id="example-button", className="mr-2"),
-            html.Span(id="example-output", style={"verticle-align": "middle"}),
-        ]
-    )
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ libs/Dash/coding/button/3_button.py
@@ -0,0 +1,25 @@
+"""
+    Buttons with outline
+"""
+import dash
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div(
+        [
+            dbc.Button("Primary", outline= True, color="primary", className="mr-1"),
+            dbc.Button("Secondary", outline= True, color="secondary", className="mr-1"),
+            dbc.Button("Success", outline= True, color="success", className="mr-1"),
+            dbc.Button("Warning", outline= True, color="warning", className="mr-1"),
+            dbc.Button("Danger", outline= True, color="danger", className="mr-1"),
+            dbc.Button("Info", outline= True, color="info", className="mr-1"),
+            dbc.Button("Light", outline= True, color="light", className="mr-1"),
+            dbc.Button("Dark", outline= True, color="dark", className="mr-1"),
+        ]
+    )
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
